c0e160bb65c0b563420c99bacb4625cf3da05dca
tygourm <90351203+tygourm@users.noreply.github.com>
2025-11-13 21:01:49 +0100

    🔧 Client tooling (#2)

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,11 +1,14 @@
 import reactLogo from "/react.svg";
 import viteLogo from "/vite.svg";
 
+import { ThemeMenu } from "@/components/atoms/theme-menu";
+
 function App() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <img src={viteLogo} />
-      <img src={reactLogo} />
+    <div className="flex h-screen justify-center items-center gap-4">
+      <img src={viteLogo} className="size-8" />
+      <ThemeMenu />
+      <img src={reactLogo} className="size-8" />
     </div>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import { viteEnvs } from "vite-envs";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +13,8 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    visualizer(),
+    viteEnvs(),
   ],
   resolve: {
     alias: {
